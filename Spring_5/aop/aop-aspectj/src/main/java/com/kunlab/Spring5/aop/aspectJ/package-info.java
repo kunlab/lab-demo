741d611d6f3d14d9bbad4bd62429bbef3dbf0242
ikun <943265266@qq.com>
2021-05-10 16:40:43 +0800

    Spring整合AspectJ基于注解实现AOP

--- Spring_5/aop/aop-aspectj/src/main/java/com/kunlab/Spring5/aop/aspectJ/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Spring + AspectJ 实现Spring AOP
- * 有2种方式：
- *      基于注解 com.kunlab.Spring5.aop.aspectJ.annotation
- *      基于配置 com.kunlab.Spring5.aop.aspectJ.xml
- * @author likun
- * @date 2021/4/30
- */
-package com.kunlab.Spring5.aop.aspectJ;
